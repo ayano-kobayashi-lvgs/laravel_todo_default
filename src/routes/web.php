830180636c6fece4b73a02c668801621242e6c0d
小林　菖乃 <ayano.kobayashi@PC-3488.local>
2022-08-09 18:26:37 +0900

    認可

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\TodoController;
+use App\Model\Todo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,10 +28,13 @@ Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
 
 Route::group(['middleware' => 'auth', 'prefix' => 'todos', 'as' => 'todos.'], function () {
     Route::get('/', [TodoController::class, 'index'])->name('index');
+    Route::get('/{id}', [TodoController::class, 'show'])->whereNumber('id')->name('show');
+    Route::post('/store', [TodoController::class, 'store'])->can('create', Todo::class)->name('store');
+    Route::put('/{id}', [TodoController::class, 'update'])->whereNumber('id')->can('update', Todo::class)->name('update');
+    Route::delete('/{id}', [TodoController::class, 'delete'])->whereNumber('id')->can('delete', Todo::class)->name('delete');
+});
+
+Route::group(['middleware' => ['auth', 'can:isAdmin'], 'prefix' => 'todos', 'as' => 'todos.'], function () {
     Route::get('/create', [TodoController::class, 'create'])->name('create');
-    Route::get('/{id}', [TodoController::class, 'show'])->name('show');
-    Route::get('/{id}/edit', [TodoController::class, 'edit'])->name('edit');
-    Route::post('/store', [TodoController::class, 'store'])->name('store');
-    Route::put('/{id}', [TodoController::class, 'update'])->name('update');
-    Route::delete('/{id}', [TodoController::class, 'delete'])->name('delete');
+    Route::get('/{id}/edit', [TodoController::class, 'edit'])->whereNumber('id')->name('edit');
 });

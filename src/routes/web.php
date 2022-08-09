c67198e753fa8379f13389975887b3459a32c04a
小林　菖乃 <ayano.kobayashi@PC-3488.local>
2022-08-09 17:59:29 +0900

    tmp

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,20 +16,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
-  Route::get('/register', [UserController::class, 'showRegister'])->name('register');
-  Route::post('/register', [UserController::class, 'executeRegister'])->name('completeRegister');
-  Route::get('/login', [LoginController::class, 'index'])->name('login');
-  Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
-  Route::post('/login', [LoginController::class, 'login'])->name('login');
+    Route::get('/register', [UserController::class, 'showRegister'])->name('register');
+    Route::post('/register', [UserController::class, 'executeRegister'])->name('completeRegister');
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
 });
 
 Route::group(['middleware' => 'auth', 'prefix' => 'todos', 'as' => 'todos.'], function () {
-  Route::get('/', [TodoController::class, 'index'])->name('index');
-  Route::get('/create', [TodoController::class, 'create'])->name('create');
-  Route::get('/{id}', [TodoController::class, 'show'])->name('show');
-  Route::get('/{id}/edit', [TodoController::class, 'edit'])->name('edit');
-  Route::post('/store', [TodoController::class, 'store'])->name('store');
-  Route::put('/{id}', [TodoController::class, 'update'])->name('update');
-  Route::delete('/{id}', [TodoController::class, 'delete'])->name('delete');
+    Route::get('/', [TodoController::class, 'index'])->name('index');
+    Route::get('/create', [TodoController::class, 'create'])->name('create');
+    Route::get('/{id}', [TodoController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [TodoController::class, 'edit'])->name('edit');
+    Route::post('/store', [TodoController::class, 'store'])->name('store');
+    Route::put('/{id}', [TodoController::class, 'update'])->name('update');
+    Route::delete('/{id}', [TodoController::class, 'delete'])->name('delete');
 });
